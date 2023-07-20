c117d99425d0e091ce5c72800c4f35760975e607
Diana Thayer <dthayer@opploans.com>
2023-07-20 12:09:00 -0700

    checkin

--- src/die_roller/core.clj
+++ src/die_roller/core.clj
@@ -1,21 +1,52 @@
 (ns die-roller.core
-  (:require [clojure.spec.alpha :as s]))
+  (:require [clojure.spec.alpha :as s]
+            [clojure.spec.gen.alpha :as g]))
 
-(s/def ::count pos-int?)
-(s/def ::faces pos-int?)
+(defn less-than-max [x] (< (abs x) Integer/MAX_VALUE))
+
+(s/def ::count (s/and pos-int? less-than-max))
+(s/def ::faces (s/and pos-int? less-than-max))
 (s/def ::roll int?)
 (s/def ::rolls (s/coll-of ::roll))
-(s/def ::best-of (s/nilable nat-int?))
-(s/def ::worst-of (s/nilable nat-int?))
-(s/def ::modifier (s/nilable int?))
-
-(s/def ::die-expr string?)
-(s/def ::parsed-expr
-  (s/nilable
-   (s/tuple ::count ::faces ::best-of ::worst-of ::modifier)))
+(s/def ::best-of (s/nilable (s/and nat-int? less-than-max)))
+(s/def ::worst-of (s/nilable (s/and nat-int? less-than-max)))
+(s/def ::modifier (s/nilable (s/and int? less-than-max)))
+(s/def ::expr-parts (s/cat :count ::count
+                           :faces ::faces
+                           :best-of ::best-of
+                           :worst-of ::worst-of
+                           :modifier ::modifier))
 
 (def expr-re #"^(\d+?)d(\d+)(b(\d+?))?(w(\d+?))?([-+]\d+)?$")
 ;; ex: 1d20, 1d20+1, 2d20b1, 5d6w3
+
+(defn make-expr [count faces best-of worst-of modifier]
+  (let [base-str (format "%sd%s" count faces)
+        best-of-str (if best-of
+                      (format "b%s" best-of)
+                      "")
+        worst-of-str (if worst-of
+                      (format "w%s" worst-of)
+                      "")
+        modifier-str (if modifier
+                      (format "%s%s"
+                              (if (< -1 modifier)
+                                "+"
+                                "")
+                              modifier)
+                      "")]
+    (str base-str best-of-str worst-of-str modifier-str)))
+
+(s/def ::die-expr
+  (s/with-gen
+    (s/and string? #(re-matches expr-re %))
+    #(g/fmap
+      (fn [args] (apply make-expr args))
+      (s/gen ::expr-parts))))
+
+(s/fdef make-expr
+  :args ::expr-parts
+  :ret ::die-expr)
 
 (defn parse-expr [expr]
   (when-let [[_ count faces _ best-of _ worst-of modifier]
@@ -26,8 +57,8 @@
       [count faces best-of worst-of modifier]))))
 
 (s/fdef parse-expr
-  :args (s/cat :expr ::die-expr)
-  :ret ::parsed-expr)
+  :args (s/cat :expr string?)
+  :ret (s/nilable ::expr-parts))
 
 (defn roll-die [n]
   (+ 1 (rand-int n)))
@@ -36,28 +67,22 @@
   :args (s/cat :n ::faces)
   :ret ::roll)
 
-(defn do-rolls [count faces best-of worst-of modifier]
-  (let [rolls
-        (sort
-         (for [_ (range count)]
-           (+ (or modifier 0)
-              (roll-die faces))))]
-    (->> rolls
-         (take-last (or best-of count))
-         (take (or worst-of count)))))
+(defn eval-rolls [count faces best-of worst-of modifier]
+  (->> (for [_ (range count)]
+         (+ (or modifier 0)
+            (roll-die faces)))
+       sort
+       (take-last (or best-of count))
+       (take (or worst-of count))))
 
-(s/fdef do-rolls
-  :args (s/cat :count ::count
-               :faces ::faces
-               :best-of ::best-of
-               :worst-of ::worst-of
-               :modifier ::modifier)
+(s/fdef eval-rolls
+  :args ::expr-parts
   :ret ::rolls)
 
-(defn do-expr [expr]
+(defn eval-die-expr [expr]
   (when-let [result (parse-expr expr)]
-    (apply do-rolls result)))
+    (apply eval-rolls result)))
 
-(s/fdef do-expr
-  :args (s/cat :expr ::die-expr)
+(s/fdef eval-die-expr
+  :args (s/cat :expr string?)
   :ret (s/nilable ::rolls))
--- test/die_roller/core_test.clj
+++ test/die_roller/core_test.clj
@@ -13,10 +13,10 @@
 
 (deftest example-example-test
   (testing "2d1 > 1d1"
-    (is (> (reduce + (core/do-expr "2d1"))
-           (reduce + (core/do-expr "1d1")))))
+    (is (> (reduce + (core/eval-die-expr "2d1"))
+           (reduce + (core/eval-die-expr "1d1")))))
   (testing "Doesn't blow up on complex input"
-    (let [result (core/do-expr "3d20b2w1-1")]
+    (let [result (core/eval-die-expr "3d20b2w1-1")]
       (and (is (= 1 (count result)))
            (is (>= 19 (first result) 0))))))
 
@@ -32,12 +32,12 @@
          worst-of 1
          expr (format "%sd%sb%sw%s-%s"
                       count* faces best-of worst-of modifier)
-         result (core/do-expr expr)]
+         result (core/eval-die-expr expr)]
      (and (is (= 1 (count result)))
           (is (>= (- faces modifier -1) (first result) (- 1 modifier)))))))
 
 ;; the generator above only uses specs sometimes
-;; because the test doesn't expect nil values
+;; because the test doesn't expect potentially-nil values
 ;; (which our specs do)
 
 ;; the following tests use specs both to generate input
@@ -46,11 +46,17 @@
 ;; because the result might be so many things!
 ;; but we can comfortably assert that the result fits data expectations.
 
+(defspec make-expr-test 500
+  (props/for-all
+   [args (s/gen ::core/expr-parts)]
+   (let [result (apply core/make-expr args)]
+     (s/valid? ::core/die-expr result))))
+
 (defspec parse-expr-test 500
   (props/for-all
    [expr (s/gen ::core/die-expr)]
    (let [result (core/parse-expr expr)]
-     (s/valid? ::core/parsed-expr result))))
+     (s/valid? ::core/expr-parts result))))
 
 (defspec roll-die-test 500
   (props/for-all
@@ -64,14 +70,10 @@
 ;; (large meaning more than a million dice with more than a million faces.)
 ;; still, it will drill down on test failures to find edge cases
 
-(defspec do-roll-test 20
+(defspec exec-roll-test 20
   (props/for-all
-   [args (s/gen (s/tuple ::core/count
-                         ::core/faces
-                         ::core/best-of
-                         ::core/worst-of
-                         ::core/modifier))]
-   (let [result (apply core/do-rolls args)]
+   [args (s/gen ::core/expr-parts)]
+   (let [result (apply core/eval-rolls args)]
      (s/valid? ::core/rolls result))))
 
 ;; this test generates strings and evaluates them as expressions,
@@ -79,7 +81,7 @@
 ;; that's fine! we've already tested everything else.
 ;; this test validates only that unruly user input won't break the system.
 
-(defspec do-expr-test 500
+(defspec exec-expr-test 500
   (props/for-all
-   [expr (s/gen ::core/die-expr)]
-   (s/valid? (s/nilable ::core/rolls) (core/do-expr expr))))
+   [expr (s/gen string?)]
+   (s/valid? (s/nilable ::core/rolls) (core/eval-die-expr expr))))
